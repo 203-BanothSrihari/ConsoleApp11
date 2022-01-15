f2fec0bd012ba306412674805f0a8a7e4fca9b38
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:04:05 +0530

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQueue = new Queue();
+            myQueue.Enqueue("hello!");
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);// by using enqueue() method we can add values 
+            myQueue.Enqueue(3);
+            myQueue.Enqueue(4);
+            myQueue.Enqueue(5);
+            myQueue.Dequeue();  //by dequeue() we can delete the data
+
+            Console.WriteLine("count the elements in the queue", myQueue.Count);
+
+            foreach (var item in myQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
